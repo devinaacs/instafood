f4f6d2bb53667d4d0e8d4499ca458573ab740b05
rafi-ramdhani <rafiramdhani20@gmail.com>
2022-02-17 17:14:22 +0700

    update: created discovery content card

--- client-mobile/screens/Discover.js
+++ client-mobile/screens/Discover.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Center, Flex, Input, ScrollView, StatusBar, Text } from 'native-base';
+import { Box, Center, Flex, Image, Input, ScrollView, StatusBar, Text } from 'native-base';
 import { AntDesign } from '@expo/vector-icons';
 
 export default function Discover() {
@@ -15,13 +15,20 @@ export default function Discover() {
         </Flex>
       </Box>
       <ScrollView>
-        <Box w={'full'} bg={'blue.200'} h={'96'}></Box>
-        <Box w={'full'} bg={'green.200'} h={'96'}></Box>
-        <Box w={'full'} bg={'red.200'} h={'96'}></Box>
-        <Box w={'full'} bg={'yellow.200'} h={'96'}></Box>
-        <Box w={'full'} bg={'orange.200'} h={'96'}></Box>
-        <Box w={'full'} bg={'pink.200'} h={'96'}></Box>
-        <Box w={'full'} bg={'blue.200'} h={'96'}></Box>
+        <Box w={'full'} maxHeight={700}>
+          <Box p={'3'} w={'full'}>
+            <Text fontSize={'xl'}>Jakarta</Text>
+          </Box>
+          <Image
+            width={'full'}
+            height={'full'}
+            resizeMode={'cover'}
+            source={{
+              uri: 'https://images.unsplash.com/photo-1529042410759-befb1204b468?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80',
+            }}
+            alt={'alternate picture'}
+          />
+        </Box>
       </ScrollView>
     </Box>
   );
